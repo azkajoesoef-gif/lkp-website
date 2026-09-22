022e21d719a4bd77b64c1883982ddbc8ee634033
azkajoesoef-gif <azkajoesoef@gmail.com>
2026-09-22 21:19:29 +0700

    Create config.js

---
+++ assets/css/js/config.js
@@ -0,0 +1,16 @@
+export const CONFIG = {
+  API_URL: '',  // diisi nanti setelah Apps Script dibuat
+
+  WHATSAPP: '6287780986660',
+  EMAIL: 'info@lingkarpedulianaknegeri.id',
+  INSTAGRAM: 'https://instagram.com/lingkarpedulianaknegeri',
+
+  ADDRESS: 'Jl. Balai Pustaka Barat No.337, Rawamangun, Jakarta Timur',
+  MAPS_LINK: '',
+  MAPS_EMBED: '',
+
+  NAMA_ORGANISASI: 'Lingkar Peduli Anak Negeri',
+  SINGKATAN: 'LKP',
+  NOMOR_SK: 'AHU-0004737.AH.01.07.TAHUN 2022',
+  PERIODE_KEPENGURUSAN: '2025–2030',
+};
